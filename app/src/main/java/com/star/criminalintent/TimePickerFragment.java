5885bac1d767aa0a49d4505dedf3114cbc0d2f0e
crystalDf <chendong333@gmail.com>
2016-09-21 22:34:16 +0800

    Upgrade to API Level 24

--- app/src/main/java/com/star/criminalintent/TimePickerFragment.java
+++ app/src/main/java/com/star/criminalintent/TimePickerFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TimePicker;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
